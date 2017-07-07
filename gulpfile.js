c08ee1f644aeb48ca3f84fd3629518de93de0836
tomas-polach <tomas@polach.ch>
2017-07-08 01:32:24 +0200

    Added build to .gitignore and gulp dev script

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const gulp = require('gulp')
 
-gulp.task('dev', require('./tasks/build').watch)
+gulp.task('dev', gulp.series(
+  require('./tasks/build').build,
+  require('./tasks/build').watch
+))
 gulp.task('build', require('./tasks/build').build)
 gulp.task('dist', require('./tasks/dist'))
